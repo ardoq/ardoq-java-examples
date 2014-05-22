53c0b6df8b347f4b4235550e0096628aa81cf3a7
anton164 <anton_abilov@hotmail.com>
2014-05-22 19:53:10 +0200

    Imports components into ardoq

--- excel-import/src/main/java/com/ardoq/ExcelImport.java
+++ excel-import/src/main/java/com/ardoq/ExcelImport.java
@@ -1,22 +1,55 @@
 package com.ardoq;
 
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.ardoq.model.*;
+import com.ardoq.service.ComponentService;
+import org.apache.poi.xssf.usermodel.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class ExcelImport {
 
-    private static final String ardoqUsername = System.getenv("ardoqUsername");
+    private static final String token = System.getenv("ardoqToken");
     private static final String host = System.getenv("ardoqHost");
-    private static final String ardoqPassword = System.getenv("ardoqPassword");
 
     public static void main(String[] args) throws IOException {
 
         InputStream resourceAsStream = ExcelImport.class.getResourceAsStream("/data.xlsx");
 
-        XSSFWorkbook xssfSheets = new XSSFWorkbook(resourceAsStream);
+        XSSFWorkbook workbook = new XSSFWorkbook(resourceAsStream);
 
-        ArdoqClient client = new ArdoqClient(host, ardoqUsername, ardoqPassword);
+        ArdoqClient client = new ArdoqClient(host, token);
+        ComponentService componentService = client.component();
+
+        Model model = client.model().getModelByName("Application service");
+        Workspace workspace = client.workspace().createWorkspace(new Workspace("excel-import", model.getId(), "Description"));
+
+        // Import components
+        XSSFSheet components = workbook.getSheet("Components");
+        int applicationColumn = 0;
+        int serviceColumn = 1;
+
+        int rowIndex = 1;
+        XSSFRow row = components.getRow(rowIndex);
+        XSSFCell applicationCell = null;
+        XSSFCell serviceCell = null;
+        Component parent = null;
+        while(row != null)
+        {
+            if (applicationCell != row.getCell(applicationColumn)) {
+                applicationCell = row.getCell(applicationColumn);
+                parent = componentService.createComponent(new Component(applicationCell.toString(), workspace.getId(), ""));
+            }
+            serviceCell = row.getCell(serviceColumn);
+            if (serviceCell != null && parent != null) {
+                String type = model.getComponentTypeByName("Service");
+                componentService.createComponent(new Component(serviceCell.toString(), workspace.getId(), type, parent.getId()));
+            }
+            rowIndex++;
+            row = components.getRow(rowIndex);
+        }
+
+        XSSFSheet tags = workbook.getSheet("Tags");
+        XSSFSheet fields = workbook.getSheet("Fields");
     }
 }
