16c9071b2620d6673de84ba2db8e1c31b1c5a0a4
anton164 <anton_abilov@hotmail.com>
2014-05-23 16:46:14 +0200

    Updated pom use v0.8.2 of the api, ExcelImport shows creation of components and integrations

--- excel-import/src/main/java/com/ardoq/ExcelImport.java
+++ excel-import/src/main/java/com/ardoq/ExcelImport.java
@@ -6,6 +6,7 @@ import org.apache.poi.xssf.usermodel.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class ExcelImport {
 
@@ -25,28 +26,58 @@ public class ExcelImport {
         Workspace workspace = client.workspace().createWorkspace(new Workspace("excel-import", model.getId(), "Description"));
 
         // Import components
-        XSSFSheet components = workbook.getSheet("Components");
-        int applicationColumn = 0;
-        int serviceColumn = 1;
+        XSSFSheet componentSheet = workbook.getSheet("Components");
+        int parentColumn = 0;
+        int childColumn = 1;
 
+        HashMap<String, String> components = new HashMap<String, String>(); // name -> id
+        XSSFRow typeRow = componentSheet.getRow(0);
         int rowIndex = 1;
-        XSSFRow row = components.getRow(rowIndex);
-        XSSFCell applicationCell = null;
-        XSSFCell serviceCell = null;
+        XSSFRow row = componentSheet.getRow(rowIndex);
+        XSSFCell parentCell = null;
+        XSSFCell childCell;
         Component parent = null;
+        Component child;
         while(row != null)
         {
-            if (applicationCell != row.getCell(applicationColumn)) {
-                applicationCell = row.getCell(applicationColumn);
-                parent = componentService.createComponent(new Component(applicationCell.toString(), workspace.getId(), ""));
+            if (components.get(row.getCell(parentColumn).toString()) == null) {
+                parentCell = row.getCell(parentColumn);
+                parent = componentService.createComponent(new Component(parentCell.toString(), workspace.getId(), "Description"));
+                components.put(parent.getName(), parent.getId());
             }
-            serviceCell = row.getCell(serviceColumn);
-            if (serviceCell != null && parent != null) {
-                String type = model.getComponentTypeByName("Service");
-                componentService.createComponent(new Component(serviceCell.toString(), workspace.getId(), type, parent.getId()));
+            childCell = row.getCell(childColumn);
+            if (childCell != null && parent != null) {
+                String typeId = model.getComponentTypeByName(typeRow.getCell(childColumn).toString());
+                child = componentService.createComponent(new Component(childCell.toString(), workspace.getId(), "Description", null, parent.getId()));
+                components.put(parent.getName() + "::" + child.getName(), child.getId());
             }
             rowIndex++;
-            row = components.getRow(rowIndex);
+            row = componentSheet.getRow(rowIndex);
+        }
+
+        // Create references
+        rowIndex = 1;
+        row = componentSheet.getRow(rowIndex);
+        while (row != null) {
+            int referenceIndex = 2;
+            XSSFCell referenceCell = row.getCell(referenceIndex);
+            while (referenceCell != null) {
+                // Get the source and target
+                parentCell = row.getCell(parentColumn);
+                childCell = row.getCell(childColumn);
+                String sourcePath = (childCell != null) ? parentCell.toString() + "::" + childCell.toString() : parentCell.toString();
+                String source = components.get(sourcePath);
+                String target = components.get(referenceCell.toString());
+
+                // Create integration
+                Reference reference = new Reference(workspace.getId(), "Description", source, target, model.getReferenceTypeByName("Synchronous"));
+                client.reference().createReference(reference);
+
+                referenceIndex++;
+                referenceCell = row.getCell(referenceIndex);
+            }
+            rowIndex++;
+            row = componentSheet.getRow(rowIndex);
         }
 
         XSSFSheet tags = workbook.getSheet("Tags");
